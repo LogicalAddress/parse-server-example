dc4f9c699cff2510dd764c060659e5613546093b
dretnan <d.retnan@nhubnigeria.com>
2016-04-10 15:30:28 +0100

    remove test

--- index.js
+++ index.js
@@ -57,12 +57,6 @@ app.get('/', function(req, res) {
   res.status(200).send('Make sure to star the parse-server repo on GitHub!');
 });
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
-
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
